cfb3cc906cfc8c7f3a28aec5270062f92d77d7bb
felipe inostroza <falberto.inostroza@gmail.com>
2021-01-29 01:02:06 -0300

    .

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, escape, request, render_template
+
+import pandas as pandas
+import joblib
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('home.html')
+
 if __name__ == '__main__':
-   app.run()
+    app.run(host = '127.0.0.1', port =5000, debug=True)
+
