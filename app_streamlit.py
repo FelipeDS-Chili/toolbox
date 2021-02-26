1a25004a8af993c6d2c81ac2a8bf917211a1d1e5
felipe inostroza <falberto.inostroza@gmail.com>
2021-02-26 00:45:18 -0300

    commit

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,10 +5,20 @@ from bs4 import BeautifulSoup
 import pandas as pandas
 import requests
 import json
+import os
 
 #title = st.text_input('Nombre del Anime', 'naruto')
 
-st.table(get_data().head())
+#st.table(get_data().head())
 
 
 
+
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+filename = file_selector()
+st.write('You selected `%s`' % filename)
