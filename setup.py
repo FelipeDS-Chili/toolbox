90c98af9c8a932e1a5ce61f069efc97f0034c2cd
felipe inostroza <falberto.inostroza@gmail.com>
2021-03-01 14:18:34 -0300

    setup solved

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 with open('requirements.txt') as f:
-    content = f.readlines()
+      content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='toolbox',
@@ -10,7 +10,7 @@ setup(name='toolbox',
       description="Project Description",
       packages=find_packages(),
       test_suite = 'tests',
-      install_requires=requirements
+      install_requires=requirements,
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/toolbox-run'],
